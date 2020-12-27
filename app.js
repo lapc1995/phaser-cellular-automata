688b4dfb6197e666317cc68158c59da28109ba80
Luis Carvalho <lapc19@hotmail.com>
2020-12-27 00:41:29 +0000

    current state

---
+++ app.js
@@ -0,0 +1,33 @@
+/*
+const config = {
+    type: Phaser.AUTO,
+    width: 1024, 
+    height: 510,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    },
+};
+
+
+const game = new Phaser.Game(config);
+
+
+function preload() {
+
+}
+
+function create() {
+   
+}
+
+function update() {
+    
+}*/
